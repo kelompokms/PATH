d9eb5e3334e752e56c1842dfae2114237a85d5fe
maroisa <ahmadroisul412@gmail.com>
2026-04-30 13:13:44 +0700

    tambah sidebar

--- frontend/src/routes/(logged)/+layout.svelte
+++ frontend/src/routes/(logged)/+layout.svelte
@@ -1,11 +1,18 @@
 <script>
 	import ArrowRight from '$lib/assets/ArrowRight.svelte';
 	import Bars3 from '$lib/assets/Bars3.svelte';
+	import Calendar from '$lib/assets/Calendar.svelte';
+	import Gear from '$lib/assets/Gear.svelte';
+	import Home from '$lib/assets/Home.svelte';
 	import PlusCircle from '$lib/assets/PlusCircle.svelte';
+	import Question from '$lib/assets/Question.svelte';
+	import Teacher from '$lib/assets/Teacher.svelte';
 	import UserCircle from '$lib/assets/UserCircle.svelte';
+	import UserGroup from '$lib/assets/UserGroup.svelte';
 	import { onMount } from 'svelte';
 
 	let { children } = $props();
+	let sidebarActive = $state(false);
 	let navItems = $state();
 
 	onMount(() => {
@@ -15,9 +22,12 @@
 
 <div class="flex flex-col">
 	<header class="flex items-center gap-2 p-3 text-center md:gap-4">
-		<checkbox class="rounded p-2 hover:bg-black/5 active:bg-black/10">
+		<button
+			class="rounded p-2 hover:bg-black/5 active:bg-black/10"
+			onclick={(sidebarActive = !sidebarActive)}
+		>
 			<Bars3 />
-		</checkbox>
+		</button>
 		<a
 			href="/beranda"
 			class="mb-1 rounded p-2 text-2xl font-black hover:bg-black/5 active:bg-black/10">PATH</a
@@ -34,8 +44,33 @@
 			<UserCircle />
 		</button>
 	</header>
-	<main class="flex grow gap-4">
-		<div class="">Lorem</div>
+	<main class="flex grow gap-4 bg-purple-100">
+		<div
+			class={`fixed h-full bg-white p-4 shadow-md transition-all md:block ${sidebarActive ? 'block' : 'hidden'}`}
+		>
+			<nav class="flex flex-col gap-4">
+				<a href="/beranda" class="flex items-center gap-2">
+					<Home />
+					<p class={`${sidebarActive ? 'block' : 'hidden'}`}>Beranda</p>
+				</a>
+				<a href="/kelas" class="flex items-center gap-2">
+					<UserGroup />
+					<p class={`${sidebarActive ? 'block' : 'hidden'}`}>Kelas</p>
+				</a>
+				<a href="/pengajar" class="flex items-center gap-2">
+					<Teacher />
+					<p class={`${sidebarActive ? 'block' : 'hidden'}`}>Pengajar</p>
+				</a>
+				<a href="/kalender" class="flex items-center gap-2">
+					<Calendar />
+					<p class={`${sidebarActive ? 'block' : 'hidden'}`}>Kalender</p>
+				</a>
+				<a href="/pengaturan" class="flex items-center gap-2">
+					<Gear />
+					<p class={`${sidebarActive ? 'block' : 'hidden'}`}>Pengaturan</p>
+				</a>
+			</nav>
+		</div>
 		<div class="grow overflow-auto">
 			{@render children()}
 		</div>
