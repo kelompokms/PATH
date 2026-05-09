f58cf99122181ef4e3d7ee98d6113e3f70d671d9
maroisa <ahmadroisul412@gmail.com>
2026-05-09 21:54:06 +0700

    tambah layout dan beranda

---
+++ frontend/src/lib/components/DrawerItem.svelte
@@ -0,0 +1,13 @@
+<script>
+    let { icon, name } = $props();
+</script>
+
+<li>
+    <button
+        class="is-drawer-close:tooltip is-drawer-close:tooltip-right hover:bg-purple-200 active:bg-purple-400 active:text-black"
+        data-tip={name}
+    >
+        {@render icon()}
+        <span class="is-drawer-close:hidden text-lg">{name}</span>
+    </button>
+</li>
---
+++ frontend/src/routes/(protected)/beranda/+page.svelte
@@ -0,0 +1,17 @@
+<main class="flex justify-center items-center">
+    <div class="flex flex-col p-4 w-full gap-2">
+        <div class="bg-white p-4 text-center">
+            <!-- Gambar Di sini -->
+            <p>Gabung Kelas atau Buat Kelas</p>
+        </div>
+        <div class="flex gap-2">
+            <button class="grow p-2 rounded-xl btn btn-primary"
+                >Gabung Kelas</button
+            >
+            <button
+                class="grow p-2 rounded-xl btn btn-primary btn-ghost border-blue-200"
+                >Buat Kelas</button
+            >
+        </div>
+    </div>
+</main>
