0a8d28efdb6088c47e9645d2e69e7b552ffaf35c
Ahmad Roisul <ahmadroisul412@gmail.com>
2026-06-01 12:09:53 +0700

    ubah adapter-node ke adapter-static

--- frontend/src/routes/(protected)/kelas/[kode]/+layout.js
+++ frontend/src/routes/(protected)/kelas/[kode]/+layout.js
@@ -1,6 +1,8 @@
 import { PUBLIC_API_URL } from "$env/static/public";
 import { get } from "$lib/utils/api";
 
+export const prerender = false;
+
 export const load = async ({ params, fetch }) => {
     const res = await fetch(PUBLIC_API_URL + "class/" + params.kode, {
         credentials: "include",
--- frontend/src/routes/(protected)/profil/+page.svelte
+++ frontend/src/routes/(protected)/profil/+page.svelte
@@ -4,7 +4,12 @@
     import { get, post } from "$lib/utils/api";
     import { onMount } from "svelte";
 
-    let { data } = $props();
+    let user = $state();
+
+    onMount(async () => {
+        const res = await get("user");
+        user = res;
+    });
 
     async function logout() {
         const res = await post("logout", {});
@@ -19,65 +24,71 @@
 </script>
 
 <main class="p-0 md:p-4">
-    <div class="bg-white">
-        <h2
-            class="text-2xl font-semibold p-4 bg-purple-200 border-2 rounded-t-lg border-purple-900/10"
-        >
-            Profil
-        </h2>
-        <div
-            class="*:flex *:justify-between *:items-center *:p-4 *:border-2 *:border-black/10 *:border-t-0 *:last:rounded-b-lg"
-        >
-            <div>
-                <div
-                    class="flex grow *:first:size-12 *:first:bg-purple-200 *:first:rounded-full gap-2 items-center"
-                >
-                    <CircleUser />
-                    <p>Foto Profil</p>
-                </div>
-                <button class="btn btn-ghost">Edit</button>
-            </div>
-            <div>
+    {#if user}
+        <div class="bg-white">
+            <h2
+                class="text-2xl font-semibold p-4 bg-purple-200 border-2 rounded-t-lg border-purple-900/10"
+            >
+                Profil
+            </h2>
+            <div
+                class="*:flex *:justify-between *:items-center *:p-4 *:border-2 *:border-black/10 *:border-t-0 *:last:rounded-b-lg"
+            >
                 <div>
-                    <h3 class="font-semibold text-lg">Display Name</h3>
-                    <p>{data.Nama}</p>
+                    <div
+                        class="flex grow *:first:size-12 *:first:bg-purple-200 *:first:rounded-full gap-2 items-center"
+                    >
+                        <CircleUser />
+                        <p>Foto Profil</p>
+                    </div>
+                    <button class="btn btn-ghost">Edit</button>
                 </div>
-                <button class="btn btn-ghost">Edit</button>
-            </div>
-            <div>
                 <div>
-                    <h3 class="font-semibold text-lg">Email</h3>
-                    <p>{data.Email}</p>
+                    <div>
+                        <h3 class="font-semibold text-lg">Display Name</h3>
+                        <p>{user.Nama}</p>
+                    </div>
+                    <button class="btn btn-ghost">Edit</button>
                 </div>
-                <button class="btn btn-ghost">Edit</button>
-            </div>
-            <div>
                 <div>
-                    <h3 class="font-semibold text-lg">Phone</h3>
-                    <p>{data.Telepon}</p>
+                    <div>
+                        <h3 class="font-semibold text-lg">Email</h3>
+                        <p>{user.Email}</p>
+                    </div>
+                    <button class="btn btn-ghost">Edit</button>
                 </div>
-                <button class="btn btn-ghost">Edit</button>
-            </div>
-            <div>
                 <div>
-                    <h3 class="font-semibold text-lg">Password</h3>
-                    <p>********</p>
+                    <div>
+                        <h3 class="font-semibold text-lg">Phone</h3>
+                        <p>{user.Telepon}</p>
+                    </div>
+                    <button class="btn btn-ghost">Edit</button>
                 </div>
-                <button class="btn btn-ghost">Edit</button>
-            </div>
+                <div>
+                    <div>
+                        <h3 class="font-semibold text-lg">Password</h3>
+                        <p>********</p>
+                    </div>
+                    <button class="btn btn-ghost">Edit</button>
+                </div>
 
-            <div>
                 <div>
-                    <h3 class="font-semibold text-lg">Jenis Kelamin</h3>
-                    <p>
-                        {data.JenisKelamin == "l" ? "Laki-laki" : "Perempuan"}
-                    </p>
+                    <div>
+                        <h3 class="font-semibold text-lg">Jenis Kelamin</h3>
+                        <p>
+                            {user.JenisKelamin == "l"
+                                ? "Laki-laki"
+                                : "Perempuan"}
+                        </p>
+                    </div>
+                    <button class="btn btn-ghost">Edit</button>
                 </div>
-                <button class="btn btn-ghost">Edit</button>
             </div>
         </div>
-    </div>
-    <button onclick={logout} class="mt-4 btn btn-error btn-outline ml-auto"
-        >Logout</button
-    >
+        <button onclick={logout} class="mt-4 btn btn-error btn-outline ml-auto"
+            >Logout</button
+        >
+    {:else}
+        <div class="w-full h-32 skeleton"></div>
+    {/if}
 </main>
